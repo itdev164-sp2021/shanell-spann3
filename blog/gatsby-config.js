b40a8a4dfa2a53bbd38d4b4baeb1f068029c7ab0
Your Nam <you@example.com>
2021-01-31 16:01:10 -0600

    Copmeted Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog`,
+    author: `Shanell Spann`,
+    contact: {
+      name: `Shanell Spann`,
+      company: `Blogs Inc.`,
+      address: `PO BOX 1234`
+    }
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
