d077cf1d9a882c5cabca86d249492c0caa932f4f
Your Nam <you@example.com>
2021-03-06 09:55:56 -0600

    Added GlobalStyle component

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import styled from 'styled-components'
 
 import Header from "./header"
-import "./layout.css"
+import GlobalStyle from './GlobalStyle'
 
 const Content = styled.div`
   margin: 0 auto;
@@ -33,6 +33,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata.title} />
       <Content>
         <main>{children}</main>
