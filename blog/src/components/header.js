f19108003c201f4488936248c2654e1c5511d7ae
Your Nam <you@example.com>
2021-03-06 09:35:36 -0600

    Header.js

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -1,6 +1,30 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from 'styled-components'
+
+const Outer = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const Inner = styled.div`
+  margin: 0px auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const H1 = styled.h1`
+  margin:0px;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  &:hover {
+    color: purple;
+  }
+`
 
 const Header = ({ siteTitle }) => (
   <header
