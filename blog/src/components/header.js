f43d5b766692a9cdcd56c8395d180faa3b1f5245
Your Nam <you@example.com>
2021-03-06 09:46:03 -0600

    Layout.js

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -27,32 +27,15 @@ const StyledLink = styled(Link)`
 `
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <Outer>
+    <Inner>
+      <H1>
+        <StyledLink to="/">
           {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+        </StyledLink>
+      </H1>
+    </Inner>
+  </Outer>
 )
 
 Header.propTypes = {
--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -8,9 +8,17 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from 'styled-components'
 
 import Header from "./header"
 import "./layout.css"
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45 rem;
+  padding-top: 0;
+  `
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,25 +33,15 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <Content>
         <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
+        <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
-      </div>
+      </Content>
     </>
   )
 }
