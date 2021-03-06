8fcaaceb6afb32b8b86ef7ee8440cb530cf031a0
Your Nam <you@example.com>
2021-03-06 10:06:36 -0600

    Completed Activity 4

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -4,7 +4,7 @@ import React from "react"
 import styled from 'styled-components'
 
 const Outer = styled.header`
-  background: rebeccapurple;
+  background: ${props => props.theme.header.backgroundColor};
   margin-bottom: 1.45rem;
 `
 
--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -8,8 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import styled from 'styled-components'
-
+import styled, { ThemeProvider } from 'styled-components'
+import { Gray } from '../themes/Gray'
 import Header from "./header"
 import GlobalStyle from './GlobalStyle'
 
@@ -32,7 +32,7 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <ThemeProvider theme={Gray}>
       <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata.title} />
       <Content>
@@ -43,7 +43,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </Content>
-    </>
+    </ThemeProvider>
   )
 }
 
